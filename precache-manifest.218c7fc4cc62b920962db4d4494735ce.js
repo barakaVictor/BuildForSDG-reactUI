5efbae29d8c2b24599e8322b97eb08a0e2165fb4
Victor Baraka <barakavictor62@gmail.com>
2020-04-11 14:12:52 +0300

    Updates

---
+++ precache-manifest.218c7fc4cc62b920962db4d4494735ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04dd3bd902931fc58319fd69b97897e6",
+    "url": "/BuildForSDG-reactUI/index.html"
+  },
+  {
+    "revision": "60c064012cb750a08b7e",
+    "url": "/BuildForSDG-reactUI/static/css/main.929308b5.chunk.css"
+  },
+  {
+    "revision": "e9ca75a0f1f95df132ba",
+    "url": "/BuildForSDG-reactUI/static/js/2.c0d66675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BuildForSDG-reactUI/static/js/2.c0d66675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c064012cb750a08b7e",
+    "url": "/BuildForSDG-reactUI/static/js/main.4601c03c.chunk.js"
+  },
+  {
+    "revision": "09ea08fc496c70c8b7b9",
+    "url": "/BuildForSDG-reactUI/static/js/runtime-main.f656051f.js"
+  }
+]);
